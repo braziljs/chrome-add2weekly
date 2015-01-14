47933b3707aff893629ef9304c2e0ae2a9f05b6c
Felquis Gimenes <frgformenton@gmail.com>
2015-01-14 01:37:56 -0200

    Remove unnecessary onInstalled event listener
    #13

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,5 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    chrome.pageAction.show(tabId);
-});
-
 chrome.tabs.onUpdated.addListener(function (tabId) {
     chrome.pageAction.show(tabId);
 });
